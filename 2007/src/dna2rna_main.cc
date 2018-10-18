2953c3fe64f0709efe932f82879c4a3d55bf00bd
Hitoshi Yoshida <github@peria.jp>
2018-10-19 08:08:02 +0900

    Make Dna2Rna accept only prefix

--- 2007/src/dna2rna_main.cc
+++ 2007/src/dna2rna_main.cc
@@ -1,12 +1,27 @@
 #include "dna2rna.h"
 
-#include <ext/rope>
+#include <glog/logging.h>
+
+#include <fstream>
 #include <iostream>
 #include <string>
+
+#ifndef ENDO_PATH
+#define ENDO_PATH "../data/endo.dna"
+#endif
 
 int main(void) {
   std::string dna;
   std::cin >> dna;
+
+  {
+    std::ifstream ifs(ENDO_PATH);
+    std::string endo;
+    ifs >> endo;
+    ifs.close();
+    dna += endo;
+  }
+  LOG(INFO) << "Total length is " << dna.size() << " bytes";
   Dna2Rna endo(dna.c_str());
   for (const Dna& rna : endo.Execute())
     std::cout << rna << "\n";
