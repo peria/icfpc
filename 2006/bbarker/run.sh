98a3bd2db71547f1549a9b3a3567f3fc41bdfed6
Hitoshi Yoshida <github@peria.jp>
2019-01-21 07:10:19 +0900

    Update black

--- 2006/bbarker/run.sh
+++ 2006/bbarker/run.sh
@@ -2,6 +2,8 @@
 
 UM_DIR=$(cd $(dirname $0)/../um; pwd)
 
-inputs=input.txt
+inputs=(input1.txt
+        000.black input_rep.txt
+        010.black input2.txt)
 
 cat ${inputs} | ${UM_DIR}/um ${UM_DIR}/umix.um | tee log.txt
