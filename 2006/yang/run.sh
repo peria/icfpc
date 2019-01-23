b946578e8273796f9908615b8e97060e80d8fdfe
Hitoshi Yoshida <github@peria.jp>
2019-01-24 01:34:14 +0900

    Refactor stop*@balance

--- 2006/yang/run.sh
+++ 2006/yang/run.sh
@@ -2,6 +2,6 @@
 
 UM_DIR=$(cd $(dirname $0)/../um; pwd)
 
-inputs=(input1.txt stop.bal input2.txt stop1.bal input3.txt)
+inputs=(input1.txt stop.bal input2.txt)
 
 cat ${inputs} | ${UM_DIR}/um ${UM_DIR}/umix.um | tee log.txt
