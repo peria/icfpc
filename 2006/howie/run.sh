c082a30bf76aa89b9acc66be025f45ed0b79ef77
Hitoshi Yoshida <github@peria.jp>
2019-01-27 23:49:27 +0900

    Solve advtr

--- 2006/howie/run.sh
+++ 2006/howie/run.sh
@@ -2,6 +2,7 @@
 
 UM_DIR=$(cd $(dirname $0)/../um; pwd)
 
-inputs=(input.txt ) # usb.adv display.adv
+inputs=(input.txt)
+#inputs=(input.txt power.adv usb.adv display.adv progress.adv)
 
 cat ${inputs} | ${UM_DIR}/um ${UM_DIR}/umix.um | tee log.txt
