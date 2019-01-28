7e6e2ee8d7d243b49e075a6f80a84dbb04902b22
Hitoshi Yoshida <github@peria.jp>
2019-01-28 23:33:48 +0900

    Get and use downloader

--- 2006/howie/run.sh
+++ 2006/howie/run.sh
@@ -2,7 +2,7 @@
 
 UM_DIR=$(cd $(dirname $0)/../um; pwd)
 
-inputs=(input.txt)
-#inputs=(input.txt power.adv usb.adv display.adv progress.adv)
+#inputs=(input.txt)
+inputs=(input.txt display.adv power.adv usb.adv progress.adv jumper.adv)
 
 cat ${inputs} | ${UM_DIR}/um ${UM_DIR}/umix.um | tee log.txt
