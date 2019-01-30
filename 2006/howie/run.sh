a6ec27c4a589e47bcb08f4af008f46c5df75569f
Hitoshi Yoshida <github@peria.jp>
2019-01-30 22:47:03 +0900

    Concat downloader.adv and add some adv

--- 2006/howie/run.sh
+++ 2006/howie/run.sh
@@ -2,7 +2,8 @@
 
 UM_DIR=$(cd $(dirname $0)/../um; pwd)
 
-#inputs=(input.txt)
-inputs=(input.txt display.adv power.adv usb.adv progress.adv jumper.adv)
+inputs=(input.txt)
+#inputs=(input.txt battery.adv rs232.adv)
+#inputs=(input.txt downloader.adv)
 
 cat ${inputs} | ${UM_DIR}/um ${UM_DIR}/umix.um | tee log.txt
