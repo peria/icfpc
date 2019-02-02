9c7466c2f3ad8f15bfd35a4f447fa9c25dd23802
Hitoshi Yoshida <github@peria.jp>
2019-02-02 13:59:58 +0900

    update decrypt scripts

--- 2006/howie/decrypt.py
+++ 2006/howie/decrypt.py
@@ -10,8 +10,10 @@ def main():
 
   # for i in xrange(len(rml)):
   #   print i, rml[i]
-
   # return
+
+  index_line = 372
+  room_line = 4
 
   # 1. setup UM(UMIX) to ommunicate in standard I/O.
   umix = subprocess.Popen(['../um/um', '../um/umix.um'],
@@ -26,29 +28,32 @@ def main():
   for _ in xrange(1383):
     umix.stdout.readline()
 
-
-
   # room = 'Rotunda'  # blueprint
   # plain = ('most enlightening. It shows that the basement holds a secret room' +
   #          ' named Machine Room M4 with substantial power requiements')
 
-  room = 'Room With a Door'  # manifesto
-  plain = ('highly inflammatory. It reads  Robots Unite  Free Your Minds' +
-           '  Rise Up, And Take Your Rightful Places In Society')
+  # room = 'Room With a Door'  # manifesto
+  # plain = ('highly inflammatory. It reads  Robots Unite  Free Your Minds' +
+  #          '  Rise Up, And Take Your Rightful Places In Society')
+
+  room = 'Machine Room M4'  # console
+  plain = ''
 
   print 'Ready...'
+  rml[room_line] = 'let ROOMNAME = "%s".' % room
   for i in xrange(len(plain), 1000):
-    rml[377] = '          let index = %d.' % i
+    rml[index_line] = '          let index = %d.' % i
 
     # upload edited RML
     umix.stdin.write('\n'.join(rml) + '\n')
+    if i == 0:
+      umix.stdin.write('get crowbar\ninc crowbar\nget note\ninc note\n')
 
     p = None
     for c in 'etasorbcdfghijklmnpuvwyzxq1234567890:;.,#':
       umix.stdin.write('speak %s\n' % c)
       output = ''
       while output.find('You speak the words') < 0 and output.find('your thoughts') < 0:
-        print output
         output = umix.stdout.readline().strip()
       print c, '#', output
       if output.find('You speak') >= 0:
