77ec857d52bddab30ce9f48d70972ebcbec358e5
Hitoshi Yoshida <github@peria.jp>
2019-07-03 21:48:26 +0900

    Implement BFS solver

--- 2019/src/point.cc
+++ 2019/src/point.cc
@@ -1,1 +1,6 @@
 #include "point.h"
+
+std::ostream& operator<<(std::ostream& os, const Point& p) {
+  os << '(' << p.x << ',' << p.y << ')';
+  return os;
+}
--- 2019/src/point.h
+++ 2019/src/point.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 struct Point {
   int x;
   int y;
@@ -15,6 +17,9 @@ struct Point {
     y += other.y;
     return *this;
   }
-  bool operator==(const Point& other) { return x == other.x && y == other.y; }
-  bool operator!=(const Point& other) { return !((*this) == other); }
+  bool operator==(const Point& other) const { return x == other.x && y == other.y; }
+  bool operator!=(const Point& other) const { return !((*this) == other); }
 };
+
+std::ostream& operator<<(std::ostream&, const Point&);
+
